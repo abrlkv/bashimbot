5b7e69d1f1b2d9f4307e094aeb3193fd458cb257
Alex Burlakov <imbrlkv@gmail.com>
2024-11-03 12:09:26 +0800

    fix 404

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import os
-
 import telebot
 
 from bashim import Bashim
@@ -13,14 +12,15 @@ keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
 button = telebot.types.KeyboardButton('Показать еще')
 keyboard.row(button)
 
-hello_message ='''
+hello_message = '''
 Бот позволяет получать цитаты с сайта башорг.рф
 Для получения рандомной цитаты, отправьте боту любое сообщение или нажмите на кнопку "Показать еще".
 Для получения конкретной цитаты отправьте id цитаты.
 '''
 quote_format = "[#{}]({}/quote/{})\n{}\n_{}_    👍 {}"
 
-bash_quote =  Bashim()
+bash_quote = Bashim()
+
 
 def get_quote(id):
     img = None
@@ -35,33 +35,36 @@ def get_quote(id):
         message = 'Ошибка получения цитаты'
     else:
         message = quote_format.format(quote['id'],
-                                    bash_quote.bashim_url,
-                                    quote['id'],
-                                    quote['text'],
-                                    quote['date'],
-                                    quote['raiting'])
+                                      bash_quote.bashim_url,
+                                      quote['id'],
+                                      quote['text'],
+                                      quote['date'],
+                                      quote['raiting'])
         img = quote['img']
-    
+
     return {'message': message, 'img': img}
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.send_message(message.chat.id,
-                    hello_message,
-                    reply_markup=keyboard)
+    bot.send_message(message.chat.id,
+                     hello_message,
+                     reply_markup=keyboard)
+
 
 @bot.message_handler(func=lambda message: True)
 def send_quote(message):
     quote = get_quote(message.text)
     if quote['img'] is not None:
         bot.send_photo(message.chat.id,
-                        quote['img'],
-                        quote['message'],
-                        reply_markup=keyboard)
+                       quote['img'],
+                       quote['message'],
+                       reply_markup=keyboard)
     else:
         bot.send_message(message.chat.id,
-                        quote['message'],
-                        disable_web_page_preview=True,
-                        reply_markup=keyboard)
+                         quote['message'],
+                         disable_web_page_preview=True,
+                         reply_markup=keyboard)
+
 
 bot.polling()
